f35c8685dcc434562340da91a0f085cba805be6c
ak1103dev <ak.apichan1103@gmail.com>
2021-08-21 16:23:28 +0700

    :construction: add swagger

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	_ "github.com/ak1103dev/go-bank-api/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 var db *gorm.DB
@@ -26,12 +29,25 @@ func initDB() {
 	db.AutoMigrate(&User{}, &Account{})
 }
 
+// @title API Document
+// @version 1.0
+// @description Description
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email soberkoder@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+
 func main() {
+	fmt.Printf("listen on 8080")
 	router = mux.NewRouter()
 	initUserRoutes()
 	initAccountRoutes()
 	// Initialize db connection
 	initDB()
 
+	router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- user.go
+++ user.go
@@ -18,6 +18,15 @@ type User struct {
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
+// userCreate godoc
+// @Summary Create a new user
+// @Description Create a new user with the input payload
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param user body User true "Create user"
+// @Success 200 {object} User
+// @Router /users [post]
 func userCreate(w http.ResponseWriter, r *http.Request) {
 	var user User
 	json.NewDecoder(r.Body).Decode(&user)
@@ -25,12 +34,22 @@ func userCreate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
+
+// userFind godoc
+// @Summary Get details of all users
+// @Description Get details of all users
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} User
+// @Router /users [get]
 func userFind(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var users []User
 	db.Find(&users)
 	json.NewEncoder(w).Encode(users)
 }
+
 func userFindById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -40,6 +59,17 @@ func userFindById(w http.ResponseWriter, r *http.Request) {
 	db.First(&user, userId)
 	json.NewEncoder(w).Encode(user)
 }
+
+// userUpdateById godoc
+// @Summary Update user identified by the given userId
+// @Description Update the user corresponding to the input userId
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param user body User true "Update user"
+// @Param userId path int true "ID of the user to be updated"
+// @Success 200 {object} User
+// @Router /users/{userId} [put]
 func userUpdateById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -55,6 +85,16 @@ func userUpdateById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
+
+// userDeleteById godoc
+// @Summary Delete user identified by the given userId
+// @Description Delete the user corresponding to the input userId
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param userId path int true "ID of the user to be deleted"
+// @Success 204 "No Content"
+// @Router /users/{userId} [delete]
 func userDeleteById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	userId := params["userId"]
